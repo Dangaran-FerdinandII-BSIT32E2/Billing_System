33e10f94dbf88f81911a690d511c1e9a6db7f6de
Teleb_JaysonEmmanuel_BSIT32E2 <157083931+Teleb-JaysonEmmanuel-BSIT32E2@users.noreply.github.com>
2024-08-23 21:04:36 +0800

    revision of frmLogin and frmRegister
    - frmLogin new design
    - frmRegister new design

---
+++ Billing System/frmLoginV2.vb
@@ -0,0 +1,12 @@
+﻿Public Class frmLoginV2
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnCreateAccount_Click(sender As Object, e As EventArgs) Handles btnCreateAccount.Click
+        frmRegisterV2.TopLevel = False
+        pnl_frmLogin.Controls.Add(frmRegisterV2)
+        frmRegisterV2.BringToFront()
+        frmRegisterV2.Show()
+    End Sub
+End Class
---
+++ Billing System/frmRegisterV2.vb
@@ -0,0 +1,12 @@
+﻿Public Class frmRegisterV2
+    Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
+        frmLoginV2.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub btnCreateAccount_Click(sender As Object, e As EventArgs) Handles btnCreateAccount.Click
+        If cboRole.SelectedIndex = 0 Then
+            MsgBox("Invalid Role! Please select your role", MsgBoxStyle.Critical, "Error")
+        End If
+    End Sub
+End Class
