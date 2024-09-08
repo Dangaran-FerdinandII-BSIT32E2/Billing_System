2351503cfcd5a350386e3f420df68a44b1bb5716
Teleb_JaysonEmmanuel_BSIT32E2 <157083931+Teleb-JaysonEmmanuel-BSIT32E2@users.noreply.github.com>
2024-09-08 20:41:43 +0800

    updated UI design for frmManageCustomer
    - added order module
    - update UI design

---
+++ Billing System/frmListofOrder.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmListofOrder
+
+End Class
---
+++ Billing System/frmManageCustomerV2.vb
@@ -0,0 +1,9 @@
+﻿Public Class frmManageCustomerV2
+    Private Sub btnViewInfo_Click(sender As Object, e As EventArgs) Handles btnViewInfo.Click
+        frmListofCustomer.Show()
+    End Sub
+
+    Private Sub btnViewOrder_Click(sender As Object, e As EventArgs) Handles btnViewOrder.Click
+        frmListofOrder.Show()
+    End Sub
+End Class
