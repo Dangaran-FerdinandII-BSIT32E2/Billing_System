2c869e7e33255b223df48ebb000f8f2232afb294
Ferdinand II Dangaran <160829247+Dangaran-FerdinandII-BSIT32E2@users.noreply.github.com>
2024-09-10 17:19:12 +0800

    update to customer v2

--- Billing System/frmManageCustomerV2.vb
+++ Billing System/frmManageCustomerV2.vb
@@ -4,6 +4,7 @@
     End Sub
 
     Private Sub btnViewOrder_Click(sender As Object, e As EventArgs) Handles btnViewOrder.Click
-        frmListofOrder.Show()
+
+        'frmListofOrder.Show()
     End Sub
 End Class
