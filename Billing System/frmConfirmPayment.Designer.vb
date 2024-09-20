6e21ed1cfff7ee1376bca12a4776f2c6f83c6991
Ferdinand II Dangaran <160829247+Dangaran-FerdinandII-BSIT32E2@users.noreply.github.com>
2024-09-20 23:19:11 +0800

    added code for image upload and retrieval, email sending

--- Billing System/frmConfirmPayment.Designer.vb
+++ Billing System/frmConfirmPayment.Designer.vb
@@ -23,7 +23,7 @@ Partial Class frmConfirmPayment
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Guna2Panel1 = New Guna.UI2.WinForms.Guna2Panel()
-        Me.txtBrowse = New Guna.UI2.WinForms.Guna2TextBox()
+        Me.pbxPayment = New Guna.UI2.WinForms.Guna2PictureBox()
         Me.btnBrowse = New Guna.UI2.WinForms.Guna2Button()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
@@ -32,6 +32,7 @@ Partial Class frmConfirmPayment
         Me.Label2 = New System.Windows.Forms.Label()
         Me.btnConfirm = New Guna.UI2.WinForms.Guna2Button()
         Me.Guna2Panel1.SuspendLayout()
+        CType(Me.pbxPayment, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.Guna2Panel2.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -40,7 +41,7 @@ Partial Class frmConfirmPayment
         Me.Guna2Panel1.BackColor = System.Drawing.Color.White
         Me.Guna2Panel1.BorderColor = System.Drawing.Color.Black
         Me.Guna2Panel1.BorderThickness = 1
-        Me.Guna2Panel1.Controls.Add(Me.txtBrowse)
+        Me.Guna2Panel1.Controls.Add(Me.pbxPayment)
         Me.Guna2Panel1.Controls.Add(Me.btnBrowse)
         Me.Guna2Panel1.Controls.Add(Me.Label3)
         Me.Guna2Panel1.Controls.Add(Me.Label1)
@@ -48,31 +49,22 @@ Partial Class frmConfirmPayment
         Me.Guna2Panel1.Controls.Add(Me.btnConfirm)
         Me.Guna2Panel1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.Guna2Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Guna2Panel1.Margin = New System.Windows.Forms.Padding(4)
         Me.Guna2Panel1.Name = "Guna2Panel1"
-        Me.Guna2Panel1.Size = New System.Drawing.Size(626, 277)
+        Me.Guna2Panel1.Size = New System.Drawing.Size(639, 711)
         Me.Guna2Panel1.TabIndex = 1
         '
-        'txtBrowse
+        'pbxPayment
         '
-        Me.txtBrowse.Cursor = System.Windows.Forms.Cursors.IBeam
-        Me.txtBrowse.DefaultText = ""
-        Me.txtBrowse.DisabledState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer))
-        Me.txtBrowse.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer))
-        Me.txtBrowse.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
-        Me.txtBrowse.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
-        Me.txtBrowse.Enabled = False
-        Me.txtBrowse.FocusedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.txtBrowse.Font = New System.Drawing.Font("Arial", 12.0!)
-        Me.txtBrowse.ForeColor = System.Drawing.Color.Black
-        Me.txtBrowse.HoverState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(213, Byte), Integer), CType(CType(218, Byte), Integer), CType(CType(223, Byte), Integer))
-        Me.txtBrowse.Location = New System.Drawing.Point(156, 135)
-        Me.txtBrowse.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.txtBrowse.Name = "txtBrowse"
-        Me.txtBrowse.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
-        Me.txtBrowse.PlaceholderText = ""
-        Me.txtBrowse.SelectedText = ""
-        Me.txtBrowse.Size = New System.Drawing.Size(388, 30)
-        Me.txtBrowse.TabIndex = 1
+        Me.pbxPayment.BackColor = System.Drawing.Color.DimGray
+        Me.pbxPayment.FillColor = System.Drawing.Color.Gray
+        Me.pbxPayment.ImageRotate = 0!
+        Me.pbxPayment.Location = New System.Drawing.Point(134, 143)
+        Me.pbxPayment.Name = "pbxPayment"
+        Me.pbxPayment.Size = New System.Drawing.Size(375, 359)
+        Me.pbxPayment.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pbxPayment.TabIndex = 10000004
+        Me.pbxPayment.TabStop = False
         '
         'btnBrowse
         '
@@ -86,10 +78,11 @@ Partial Class frmConfirmPayment
         Me.btnBrowse.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold)
         Me.btnBrowse.ForeColor = System.Drawing.Color.White
         Me.btnBrowse.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left
-        Me.btnBrowse.Location = New System.Drawing.Point(36, 132)
+        Me.btnBrowse.Location = New System.Drawing.Point(251, 542)
+        Me.btnBrowse.Margin = New System.Windows.Forms.Padding(4)
         Me.btnBrowse.Name = "btnBrowse"
         Me.btnBrowse.PressedDepth = 20
-        Me.btnBrowse.Size = New System.Drawing.Size(109, 36)
+        Me.btnBrowse.Size = New System.Drawing.Size(145, 44)
         Me.btnBrowse.TabIndex = 1
         Me.btnBrowse.Text = "Browse"
         '
@@ -99,9 +92,10 @@ Partial Class frmConfirmPayment
         Me.Label3.BackColor = System.Drawing.Color.Transparent
         Me.Label3.Font = New System.Drawing.Font("Arial", 12.0!)
         Me.Label3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(57, Byte), Integer), CType(CType(18, Byte), Integer), CType(CType(116, Byte), Integer))
-        Me.Label3.Location = New System.Drawing.Point(196, 92)
+        Me.Label3.Location = New System.Drawing.Point(168, 99)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(244, 18)
+        Me.Label3.Size = New System.Drawing.Size(310, 23)
         Me.Label3.TabIndex = 10000003
         Me.Label3.Text = "Upload picture to confirm payment"
         '
@@ -111,9 +105,10 @@ Partial Class frmConfirmPayment
         Me.Label1.BackColor = System.Drawing.Color.Transparent
         Me.Label1.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold)
         Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(57, Byte), Integer), CType(CType(18, Byte), Integer), CType(CType(116, Byte), Integer))
-        Me.Label1.Location = New System.Drawing.Point(247, 63)
+        Me.Label1.Location = New System.Drawing.Point(247, 66)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(122, 19)
+        Me.Label1.Size = New System.Drawing.Size(149, 24)
         Me.Label1.TabIndex = 10000002
         Me.Label1.Text = "Upload Picture"
         '
@@ -126,8 +121,9 @@ Partial Class frmConfirmPayment
         Me.Guna2Panel2.CustomBorderThickness = New System.Windows.Forms.Padding(1, 1, 1, 0)
         Me.Guna2Panel2.Dock = System.Windows.Forms.DockStyle.Top
         Me.Guna2Panel2.Location = New System.Drawing.Point(0, 0)
+        Me.Guna2Panel2.Margin = New System.Windows.Forms.Padding(4)
         Me.Guna2Panel2.Name = "Guna2Panel2"
-        Me.Guna2Panel2.Size = New System.Drawing.Size(626, 50)
+        Me.Guna2Panel2.Size = New System.Drawing.Size(639, 62)
         Me.Guna2Panel2.TabIndex = 42
         '
         'btnExit
@@ -143,11 +139,12 @@ Partial Class frmConfirmPayment
         Me.btnExit.FillColor = System.Drawing.Color.Transparent
         Me.btnExit.Font = New System.Drawing.Font("Arial", 16.0!, System.Drawing.FontStyle.Bold)
         Me.btnExit.ForeColor = System.Drawing.Color.White
-        Me.btnExit.Location = New System.Drawing.Point(573, 0)
+        Me.btnExit.Location = New System.Drawing.Point(568, 0)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(4)
         Me.btnExit.Name = "btnExit"
         Me.btnExit.PressedColor = System.Drawing.Color.FromArgb(CType(CType(50, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(130, Byte), Integer))
         Me.btnExit.PressedDepth = 50
-        Me.btnExit.Size = New System.Drawing.Size(53, 50)
+        Me.btnExit.Size = New System.Drawing.Size(71, 62)
         Me.btnExit.TabIndex = 10000001
         Me.btnExit.TabStop = False
         Me.btnExit.Text = "x"
@@ -159,11 +156,12 @@ Partial Class frmConfirmPayment
         Me.Label2.BackColor = System.Drawing.Color.Transparent
         Me.Label2.Font = New System.Drawing.Font("Arial", 14.0!, System.Drawing.FontStyle.Bold)
         Me.Label2.ForeColor = System.Drawing.Color.White
-        Me.Label2.Location = New System.Drawing.Point(11, 13)
+        Me.Label2.Location = New System.Drawing.Point(15, 16)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(170, 22)
+        Me.Label2.Size = New System.Drawing.Size(269, 29)
         Me.Label2.TabIndex = 33
-        Me.Label2.Text = "Confirm Payment"
+        Me.Label2.Text = "Payment Confirmation"
         '
         'btnConfirm
         '
@@ -177,24 +175,27 @@ Partial Class frmConfirmPayment
         Me.btnConfirm.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold)
         Me.btnConfirm.ForeColor = System.Drawing.Color.White
         Me.btnConfirm.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left
-        Me.btnConfirm.Location = New System.Drawing.Point(215, 209)
+        Me.btnConfirm.Location = New System.Drawing.Point(251, 605)
+        Me.btnConfirm.Margin = New System.Windows.Forms.Padding(4)
         Me.btnConfirm.Name = "btnConfirm"
         Me.btnConfirm.PressedDepth = 20
-        Me.btnConfirm.Size = New System.Drawing.Size(188, 36)
+        Me.btnConfirm.Size = New System.Drawing.Size(145, 44)
         Me.btnConfirm.TabIndex = 41
-        Me.btnConfirm.Text = "Confirm"
+        Me.btnConfirm.Text = "Upload"
         '
         'frmConfirmPayment
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(626, 277)
+        Me.ClientSize = New System.Drawing.Size(639, 711)
         Me.Controls.Add(Me.Guna2Panel1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "frmConfirmPayment"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Guna2Panel1.ResumeLayout(False)
         Me.Guna2Panel1.PerformLayout()
+        CType(Me.pbxPayment, System.ComponentModel.ISupportInitialize).EndInit()
         Me.Guna2Panel2.ResumeLayout(False)
         Me.Guna2Panel2.PerformLayout()
         Me.ResumeLayout(False)
@@ -208,5 +209,5 @@ Partial Class frmConfirmPayment
     Friend WithEvents Label3 As Label
     Friend WithEvents Label1 As Label
     Friend WithEvents btnBrowse As Guna.UI2.WinForms.Guna2Button
-    Friend WithEvents txtBrowse As Guna.UI2.WinForms.Guna2TextBox
+    Friend WithEvents pbxPayment As Guna.UI2.WinForms.Guna2PictureBox
 End Class
--- Billing System/frmManageCollection.vb
+++ Billing System/frmManageCollection.vb
@@ -3,11 +3,16 @@ Imports MySql.Data.MySqlClient
 Public Class frmManageCollection
     Private Sub frmManageCollection_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Call connection()
-        TabControl2.SelectedTab = Collection
+        TabControl2.SelectedTab = DeliveryDetails
     End Sub
     Private Sub btnShow_Click_2(sender As Object, e As EventArgs) Handles btnShow.Click
         frmManageInvoice.Show()
     End Sub
 
+    Private Sub btnView_Click(sender As Object, e As EventArgs) Handles btnView.Click
 
+    End Sub
+    Private Sub btnConfirm_Click(sender As Object, e As EventArgs) Handles btnConfirm.Click
+        frmConfirmPayment.ShowDialog()
+    End Sub
 End Class
