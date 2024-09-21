c4fce8a0b0e8d473510489f3d156d9344a647a8d
Teleb_JaysonEmmanuel_BSIT32E2 <157083931+Teleb-JaysonEmmanuel-BSIT32E2@users.noreply.github.com>
2024-09-21 13:09:15 +0800

    fixed UI design and added new forms
    - added frmConfirmDelivery
    - added frmPrintBillingStatement
    - fixed UI design

--- Billing System/frmConfirmDelivery.Designer.vb
+++ Billing System/frmConfirmDelivery.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class frmConfirmPayment
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmConfirmDelivery
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()>
+    <System.Diagnostics.DebuggerNonUserCode()> _
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,9 +20,9 @@ Partial Class frmConfirmPayment
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()>
+    <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmConfirmPayment))
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmConfirmDelivery))
         Me.panelBody = New System.Windows.Forms.Panel()
         Me.btnConfirm = New Guna.UI2.WinForms.Guna2Button()
         Me.Panel2 = New System.Windows.Forms.Panel()
@@ -56,7 +56,7 @@ Partial Class frmConfirmPayment
         Me.panelBody.Location = New System.Drawing.Point(0, 0)
         Me.panelBody.Name = "panelBody"
         Me.panelBody.Size = New System.Drawing.Size(627, 518)
-        Me.panelBody.TabIndex = 1
+        Me.panelBody.TabIndex = 2
         '
         'btnConfirm
         '
@@ -212,19 +212,18 @@ Partial Class frmConfirmPayment
         Me.Label2.ForeColor = System.Drawing.Color.White
         Me.Label2.Location = New System.Drawing.Point(10, 14)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(170, 22)
+        Me.Label2.Size = New System.Drawing.Size(165, 22)
         Me.Label2.TabIndex = 34
-        Me.Label2.Text = "Confirm Payment"
+        Me.Label2.Text = "Confirm Delivery"
         '
-        'frmConfirmPayment
+        'frmConfirmDelivery
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(627, 518)
         Me.Controls.Add(Me.panelBody)
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.MaximizeBox = False
-        Me.Name = "frmConfirmPayment"
+        Me.Name = "frmConfirmDelivery"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.panelBody.ResumeLayout(False)
         Me.panelBody.PerformLayout()
--- Billing System/frmConfirmPayment.vb
+++ Billing System/frmConfirmPayment.vb
@@ -1,6 +1,5 @@
 ﻿Public Class frmConfirmPayment
-
-    Private Sub btnExit_Click_1(sender As Object, e As EventArgs) Handles btnExit.Click
+    Private Sub btnExit_Click_1(sender As Object, e As EventArgs)
         Me.Close()
     End Sub
 End Class
